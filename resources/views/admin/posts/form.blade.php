faede75711f439d40731dcce138f0f9ef5babb4a
Marcel Pociot <m.pociot@gmail.com>
2018-06-01 12:08:37 +0200

    first commit

---
+++ resources/views/admin/posts/form.blade.php
@@ -0,0 +1,29 @@
+<div class="form-group {{ $errors->has('title') ? 'has-error' : ''}}">
+    <label for="title" class="col-md-4 control-label">{{ 'Title' }}</label>
+    <div class="col-md-6">
+        <input class="form-control" name="title" type="text" id="title" value="{{ $post->title or ''}}" >
+        {!! $errors->first('title', '<p class="help-block">:message</p>') !!}
+    </div>
+</div><div class="form-group {{ $errors->has('content') ? 'has-error' : ''}}">
+    <label for="content" class="col-md-4 control-label">{{ 'Content' }}</label>
+    <div class="col-md-6">
+        <textarea class="form-control" rows="5" name="content" type="textarea" id="content" >{{ $post->content or ''}}</textarea>
+        {!! $errors->first('content', '<p class="help-block">:message</p>') !!}
+    </div>
+</div><div class="form-group {{ $errors->has('category') ? 'has-error' : ''}}">
+    <label for="category" class="col-md-4 control-label">{{ 'Category' }}</label>
+    <div class="col-md-6">
+        <select name="category" class="form-control" id="category" >
+    @foreach (json_decode('{"technology": "Technology", "tips": "Tips", "health": "Health"}', true) as $optionKey => $optionValue)
+        <option value="{{ $optionKey }}" {{ (isset($post->category) && $post->category == $optionKey) ? 'selected' : ''}}>{{ $optionValue }}</option>
+    @endforeach
+</select>
+        {!! $errors->first('category', '<p class="help-block">:message</p>') !!}
+    </div>
+</div>
+
+<div class="form-group">
+    <div class="col-md-offset-4 col-md-4">
+        <input class="btn btn-primary" type="submit" value="{{ $submitButtonText or 'Create' }}">
+    </div>
+</div>
---
+++ resources/views/admin/posts/show.blade.php
@@ -0,0 +1,40 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            @include('admin.sidebar')
+
+            <div class="col-md-9">
+                <div class="card">
+                    <div class="card-header">Post {{ $post->id }}</div>
+                    <div class="card-body">
+
+                        <a href="{{ url('/admin/posts') }}" title="Back"><button class="btn btn-warning btn-sm"><i class="fa fa-arrow-left" aria-hidden="true"></i> Back</button></a>
+                        <a href="{{ url('/admin/posts/' . $post->id . '/edit') }}" title="Edit Post"><button class="btn btn-primary btn-sm"><i class="fa fa-pencil-square-o" aria-hidden="true"></i> Edit</button></a>
+
+                        <form method="POST" action="{{ url('admin/posts' . '/' . $post->id) }}" accept-charset="UTF-8" style="display:inline">
+                            {{ method_field('DELETE') }}
+                            {{ csrf_field() }}
+                            <button type="submit" class="btn btn-danger btn-sm" title="Delete Post" onclick="return confirm(&quot;Confirm delete?&quot;)"><i class="fa fa-trash-o" aria-hidden="true"></i> Delete</button>
+                        </form>
+                        <br/>
+                        <br/>
+
+                        <div class="table-responsive">
+                            <table class="table">
+                                <tbody>
+                                    <tr>
+                                        <th>ID</th><td>{{ $post->id }}</td>
+                                    </tr>
+                                    <tr><th> Title </th><td> {{ $post->title }} </td></tr><tr><th> Content </th><td> {{ $post->content }} </td></tr><tr><th> Category </th><td> {{ $post->category }} </td></tr>
+                                </tbody>
+                            </table>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
